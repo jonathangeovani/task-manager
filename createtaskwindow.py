6681b079854826b5bd4b3fc1b5e9e98e64193728
jonathangeovani <dongeovanimenor@gmail.com>
2024-07-17 13:53:31 -0300

    fix: add taskDate field

--- createtaskwindow.py
+++ createtaskwindow.py
@@ -1,18 +1,21 @@
 import tkinter as tk
 from tkinter import ttk, messagebox, StringVar, IntVar, BooleanVar
-from utils import load_config, save_config, validate_alarm, validate_alarm_advance
+from utils import *
 import re
 
 
 class CreateTaskWindow(tk.Toplevel):
   def __init__(self) -> None:
     super().__init__()
+    today_date = stringify_date(get_curr_date())
+
     self.title('Create new Task')
     self.config(padx=10, pady=10)
     self.columnconfigure(2, weight=3)
     self.resizable(False, False)
 
     self.setup = load_config()
+    self.date_checker = self.register(validate_date)
     self.alarm_checker = self.register(validate_alarm)
     self.alarm_advance_checker = self.register(validate_alarm_advance)
 
@@ -29,27 +32,33 @@ class CreateTaskWindow(tk.Toplevel):
     self.taskDescriptionLabel = ttk.Label(self, text='Descrição:').grid(row=2, column=0, padx=(10, 5), pady=(5, 0), sticky='w')
     self.taskDescriptionEntry = ttk.Entry(self, textvariable=self.taskDescriptionValue, font=('Arial', 13)).grid(row=2, column=1, columnspan=2, padx=(5, 10), pady=(5, 0), sticky='nsew')
 
+    self.taskDateValue = StringVar(self, value=today_date)
+    self.taskDateValue.trace_add('write', self.format_date_value)
+    self.taskDateLabel = ttk.Label(self, text='Dia:').grid(row=3, column=0, padx=(10, 5), pady=(5, 0), sticky='w')
+    self.taskDateEntry = ttk.Entry(self, textvariable=self.taskDateValue, validate='key', validatecommand=(self.date_checker, '%P'), font=('Arial', 13), width=10)
+    self.taskDateEntry.grid(row=3, column=1, columnspan=2, padx=(5, 10), pady=(5, 0), sticky='w')
+
     self.taskAlarmValue = StringVar(self, value='09:00')
     self.taskAlarmValue.trace_add('write', self.format_alarm_value)
-    self.taskAlarmLabel = ttk.Label(self, text='Horário:').grid(row=3, column=0, padx=(10, 5), pady=(5, 0), sticky='w')
+    self.taskAlarmLabel = ttk.Label(self, text='Horário:').grid(row=4, column=0, padx=(10, 5), pady=(5, 0), sticky='w')
     self.taskAlarmEntry = ttk.Entry(self, textvariable=self.taskAlarmValue, validate='key', validatecommand=(self.alarm_checker, '%P'), font=('Arial', 13), width=5, justify='center')
-    self.taskAlarmEntry.grid(row=3, column=1, columnspan=2, padx=(5, 10), pady=(5, 0), sticky='w')
+    self.taskAlarmEntry.grid(row=4, column=1, columnspan=2, padx=(5, 10), pady=(5, 0), sticky='w')
 
     self.useCustomTaskAlarmAdvanceValue = BooleanVar(self, value=False)
     self.taskAlarmAdvanceCheckbox = ttk.Checkbutton(self, text='Personalizar antecedência', variable=self.useCustomTaskAlarmAdvanceValue, command=self.toggle_task_alarm_advance_state)
-    self.taskAlarmAdvanceCheckbox.grid(row=4, column=0, columnspan=3, padx=(10, 5), pady=(20, 0), sticky='w')
+    self.taskAlarmAdvanceCheckbox.grid(row=5, column=0, columnspan=3, padx=(10, 5), pady=(20, 0), sticky='w')
 
     self.taskAlarmAdvanceValue = IntVar(self, value=self.setup.get('default_advance'))
     self.taskAlarmAdvanceValue.trace_add('write', self.format_alarm_advance_value)
     self.taskAlarmAdvanceLabel1 = ttk.Label(self, text='Avisar', state='disabled')
-    self.taskAlarmAdvanceLabel1.grid(row=5, column=0, padx=(10, 0), pady=(5, 0), sticky='e')
+    self.taskAlarmAdvanceLabel1.grid(row=6, column=0, padx=(10, 0), pady=(5, 0), sticky='e')
     self.taskAlarmAdvanceEntry = ttk.Entry(self, textvariable=self.taskAlarmAdvanceValue, validate='key', validatecommand=(self.alarm_advance_checker, '%P'), font=('Arial', 13), width=2, justify='center', state='disabled')
-    self.taskAlarmAdvanceEntry.grid(row=5, column=1, padx=5, pady=(5, 0), sticky='w')
+    self.taskAlarmAdvanceEntry.grid(row=6, column=1, padx=5, pady=(5, 0), sticky='w')
     self.taskAlarmAdvanceLabel2 = ttk.Label(self, text='dias antes', state='disabled')
-    self.taskAlarmAdvanceLabel2.grid(row=5, column=2, padx=(0, 5), pady=(5, 0), sticky='w')
+    self.taskAlarmAdvanceLabel2.grid(row=6, column=2, padx=(0, 5), pady=(5, 0), sticky='w')
 
-    self.createTaskButton = ttk.Button(self, text='Criar Tarefa', command=self.save_new_task).grid(row=6, column=0, padx=(10, 5), pady=(15, 10), sticky='w')
-    self.cancelButton = ttk.Button(self, text='Cancelar', command=self.destroy).grid(row=6, column=1, columnspan=2, padx=(0, 10), pady=(15, 10), sticky='w')
+    self.createTaskButton = ttk.Button(self, text='Criar Tarefa', command=self.save_new_task).grid(row=7, column=0, padx=(10, 5), pady=(15, 10), sticky='w')
+    self.cancelButton = ttk.Button(self, text='Cancelar', command=self.destroy).grid(row=7, column=1, columnspan=2, padx=(0, 10), pady=(15, 10), sticky='w')
 
 
   def toggle_task_alarm_advance_state(self) -> None:
@@ -85,7 +94,7 @@ class CreateTaskWindow(tk.Toplevel):
           self.taskAlarmEntry.insert(3, '0')
 
 
-  def format_alarm_advance_value(self, *args) -> bool:
+  def format_alarm_advance_value(self, *args) -> None:
     text = self.taskAlarmAdvanceEntry.get()
     
     if len(text) == 2:
@@ -93,6 +102,10 @@ class CreateTaskWindow(tk.Toplevel):
     elif len(text) == 0:
       self.taskAlarmAdvanceEntry.insert(0, '0')
       self.taskAlarmAdvanceEntry.select_range(0,1)
+
+  
+  def format_date_value(self, *args) -> None:
+    ...
 
 
   def save_new_task(self) -> None:
@@ -128,6 +141,6 @@ class CreateTaskWindow(tk.Toplevel):
     self.setup['tasks'].append(new_task)
     save_config(self.setup)
     self.destroy()
-    
+
 
 # CreateTaskWindow().mainloop()
